f952587c616ad548a1b39eca482e7dabf0dd1c1a
Kirk Lauri <kirk.lauri@live.com>
2025-02-17 16:58:56 -0500

    started working on flask app

--- run_client.py
+++ run_client.py
@@ -1,74 +1,73 @@
-import torchaudio
-from pyannote.audio.pipelines.utils.hook import ProgressHook
-from pyannote.audio import Pipeline
-import whisper
-from pydub import AudioSegment
-# from whisper_live.client import TranscriptionClient
-from datetime import date
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
-from uuid import uuid4
+from logging.config import dictConfig
+import logging
 
 
-# Load pipeline from disk
-pipeline = Pipeline.from_pretrained("models/config.yaml")
-model = whisper.load_model('base.en')
+UPLOAD_FOLDER = './uploaded_audio'
+ALLOWED_EXTENSIONS = {'mp3', 'wav', 'mp4'}
 
 
-def run_pyannote(audio_file: str):
-    waveform, sample_rate = torchaudio.load(audio_file)
-
-    # run the pipeline on an audio file
-    with ProgressHook() as hook:
-        diarization = pipeline({
-            "waveform": waveform,
-            "sample_rate": sample_rate
-            }, hook=hook
-        )
-    # dump the diarization output to disk using RTTM format
-    rttm_filename = f"{date.today()}-audio-output.rttm"
-    with open(rttm_filename, "w") as rttm:
-        print(diarization)
-        diarization.write_rttm(rttm)
-    return rttm_filename
-
-
-def create_segments(rttm_file: str):
-    print(f'Creating segments for {rttm_file}')
-    with open(rttm_file, 'r') as rttm:
-        lines = rttm.readlines()
-
-    last_speaker = ''
-    segment_number = 0
-    transcription_id = uuid4()
-
-    for line in lines:
-        words = line.split(' ')
-        print(words)
-        time_start = words[3]
-        duration = words[4]
-        speaker = words[7]
-        audio = AudioSegment.from_wav('audio_files/audio.wav')
-        segment_start = float(time_start) * 1000
-        segment_end = (float(time_start) + float(duration)) * 1000
-        print(time_start, segment_start)
-        segment = audio[segment_start: segment_end]
-        segment.export(f'segments/{segment_number}-segment.wav', format='wav')
-        transcription = transcribe_segment(f'segments/{segment_number}-segment.wav')
-        with open(f'{transcription_id}-new_transcription.txt', 'a') as file:
-            if speaker != last_speaker:
-                file.write(f'\n{speaker}: {transcription}')
-            else:
-                file.write(f'{transcription}')
-        last_speaker = speaker
-
-    return segment_number
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 
-def transcribe_segment(segment: str):
-    result = model.transcribe(segment)
-    return result['text']
+app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-if __name__ == '__main__':
-    rttm_filename = run_pyannote('audio_files\\audio.wav')
-    number_of_segments = create_segments('2025-02-04-audio-output.rttm')
+def allowed_file(filename):
+    app.logger.info('checkin on the filename')
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    app.logger.info(request.method)
+    if request.method == 'POST':
+        app.logger.info('attempting to upload new file')
+        if 'file' not in request.files:
+            flash('No file part')
+            app.logger.warning('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected files')
+            app.logger.warning('No selected files')
+            return redirect(request.url)
+        app.logger.info(f'Is file allowed: {allowed_file(file.filename)}')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            app.logger.info(f'saved file to {filename}')
+            return redirect(url_for('index', name=filename))
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+        </form>
+    '''
+
+
+@app.route('/')
+def index():
+    return '<html><body>Hello!</body></html>'
